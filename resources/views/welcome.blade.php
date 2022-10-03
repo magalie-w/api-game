ca77186dfec71706272cf8ec25f75e858904aae0
magalie <magalie.waligora@gmail.com>
2022-10-03 16:17:35 +0200

    fusion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,9 @@
 
         <title>Laravel</title>
 
+        @viteReactRefresh
+        @vite('resources/js/app.jsx')
+        
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
@@ -21,6 +24,7 @@
         </style>
     </head>
     <body class="antialiased">
+        <div id="app">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
@@ -128,5 +132,6 @@
                 </div>
             </div>
         </div>
+    </div>
     </body>
 </html>
